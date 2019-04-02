8f8ea0a493fce740cdbf0727f535e203f7fe4267
UseK <use.k.james@gmail.com>
2019-04-02 22:59:15 +0900

    Just implement isOK() for abc122_d

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -13,6 +13,25 @@ object D {
   def solve(input: Array[String]) = {
 
   }
+
+  def isOK(last4: String) = {
+    unContainsAGC(last4) &&
+      unContainsAGC(swapAdjacent(last4, 0)) &&
+      unContainsAGC(swapAdjacent(last4, 1)) &&
+      unContainsAGC(swapAdjacent(last4, 2))
+  }
+
+  def unContainsAGC(s: String) = {
+    !s.contains("AGC")
+  }
+
+  def swapAdjacent(last4: String, idx: Int) = {
+    val sArray = last4.toArray
+    val tmp = sArray(idx)
+    sArray.update(idx, sArray(idx+1))
+    sArray.update(idx+1, tmp)
+    sArray.mkString("")
+  }
 }
 
 
--- src/test/scala/DTest.scala
+++ src/test/scala/DTest.scala
@@ -1,6 +1,14 @@
+import main.D
 import org.scalatest.FunSuite
 class DTest extends FunSuite {
   test("debug") {
+    assert(!D.isOK("AGCX"))
+    assert(!D.isOK("AGXC"))
+  }
 
+  test("swapAdjacent") {
+    assert(D.swapAdjacent("ABCD", 0) == "BACD")
+    assert(D.swapAdjacent("ABCD", 1) == "ACBD")
+    assert(D.swapAdjacent("ABCD", 2) == "ABDC")
   }
 }
